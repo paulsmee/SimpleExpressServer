3563043a22ddbbadad37e5a7c48fb577246736d1
paul <paulsmee.is@gmail.com>
2017-12-06 19:22:40 +1100

    Update

--- router.js
+++ router.js
@@ -1,11 +1,12 @@
 // This file will handle all the routing for the server
+'use-strict'
 
-var express = require('express');
-var router = express.Router();
+var express = require('express') // This will load the 'express' package so that it can be used with this file.
+var router = express.Router() // Create a variable so we can use the 'Router' function within the 'express' package.
 
-router.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-  console.log("Access Detected!");
+router.get('/', function(req, res) { // When the root webpage i.e. http://localhost:8000/ is called the function is run giving the below 'res'ponse.
+  res.sendFile(__dirname + '/index.html') //Web page root directory and the name of the file to display to the users browser.
+  console.log("Access Detected!") //Displays this alert on the server each time the page is accessed.
 });
 
-module.exports = router
+module.exports = router // This will export the functions to be able to be used in our server.js file.
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var server = express();
+// This is the base server which also routes functions to the user.
+'use-strict' // 'use-strict' will ensure the file will fail if there are any errors, it's a quick way to ensure your code is correct (though it still might not do what you expected).
 
-server.use(express.static('public'));
-server.use(require('./router'));
+var express = require('express') // This loads the 'express' package so we can use the functions within it.
+var server = express()
 
-var port = 8000
-server.listen(port, function() {
-  console.log('The server is listening on port ' + port);
-});
+server.use(express.static('public'))
+server.use(require('./router')) // Connect our router function from router.js (the module we exported at the bottom).
+
+var port = 8000 // Specify which port to listen on
+server.listen(port, function() {  //This is the action our server will take once it is ready to serve.
+  console.log('The server is listening on port ' + port) // This message is displayed on the host machine when the server is started.
+})
