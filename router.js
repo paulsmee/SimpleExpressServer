55e3b31254358df31eb528501bc79151bd646d90
paul <paulsmee.is@gmail.com>
2017-12-06 19:54:21 +1100

    Update

--- router.js
+++ router.js
@@ -1,12 +1,18 @@
 // This file will handle all the routing for the server
-'use-strict'
+'use strict'
 
-var express = require('express') // This will load the 'express' package so that it can be used with this file.
-var router = express.Router() // Create a variable so we can use the 'Router' function within the 'express' package.
+// This will load the 'express' package so that it can be used with this file.
+var express = require('express')
+// Create a variable so we can use the 'Router' function within the 'express'package.
+var router = express.Router()
 
-router.get('/', function(req, res) { // When the root webpage i.e. http://localhost:8000/ is called the function is run giving the below 'res'ponse.
-  res.sendFile(__dirname + '/index.html') //Web page root directory and the name of the file to display to the users browser.
-  console.log("Access Detected!") //Displays this alert on the server each time the page is accessed.
+// When the root webpage i.e. http://localhost:8000/ is called the function is run giving the below 'res'ponse.
+router.get('/', function(req, res) {
+  //Web page root directory and the name of the file to display to the users browser.
+  res.sendFile(__dirname + '/index.html')
+  //Displays this alert on the server each time the page is accessed.
+  console.log("Access Detected!")
 });
 
-module.exports = router // This will export the functions to be able to be used in our server.js file.
+// This will export the functions to be able to be used in our server.js file.
+module.exports = router
